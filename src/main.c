7c489d50542d275f0e51af92fc32eff6a4be8a9d
Sergej Sawazki <ce3a@gmx.de>
2022-11-01 21:13:13 +0100

    Introduce indication handling

--- src/main.c
+++ src/main.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
+#include "hardware/gpio.h"
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
 
-#define RF_PIN_LEVEL_1          1u
-#define RF_PIN_LEVEL_2          2u
-#define RF_PIN_LEVEL_3          3u
-#define RF_PIN_ABSENT           4u
-#define RF_PIN_AUTO             5u
-#define RF_PIN_TIME             6u
+#define RF_PIN_LEVEL_1              1u
+#define RF_PIN_LEVEL_2              2u
+#define RF_PIN_LEVEL_3              3u
+#define RF_PIN_ABSENT               4u
+#define RF_PIN_AUTO                 5u
+#define RF_PIN_TIME                 6u
 
-#define RF_DELAY_SHORT_PULSE  250u  /* ms */
-#define RF_DELAY_INTER_PULSE  250u  /* ms */
+#define RF_LED_PIN_RED             26u
+#define RF_LED_PIN_GREEN           27u
+
+#define RF_DELAY_SHORT_PULSE      250u  /* ms */
+#define RF_DELAY_INTER_PULSE      250u  /* ms */
+
+#define RF_DELAY_INDICATION     10000u  /* ms */
 
 struct rf_command {
     char key;           /**< Command invocation character */
@@ -22,9 +28,15 @@ struct rf_command {
     const char* help;
 };
 
+struct rf_response {
+    uint green;         /**< Number of green LED pules */
+    uint red;           /**< Number of red LED pules */
+};
+
 enum rf_result {
     RF_OK,              /**< Success */
     RF_E_CMD,           /**< Invalid command */
+    RF_E_RSP,           /**< Unexpected response (LED indication) */
     RF_E_COM,           /**< RF communication error */
     RF_E_DEV            /**< Device error */
 };
@@ -38,6 +50,11 @@ static const uint rf_pins[] = {
     RF_PIN_LEVEL_3,     /**< GPIO for Mode high(3) mode */
 };
 
+static const uint rf_leds[] = {
+    RF_LED_PIN_RED,     /**< GPIO for Red LED */
+    RF_LED_PIN_GREEN    /**< GPIO for Green LED */
+};
+
 static const struct rf_command rf_cmds[] = {
     {'1', RF_PIN_LEVEL_1, 1, "Low power mode"},
     {'2', RF_PIN_LEVEL_2, 1, "Mid power mode"},
@@ -49,8 +66,23 @@ static const struct rf_command rf_cmds[] = {
     {'e', RF_PIN_ABSENT,  1, "Absence mode"}
 };
 
+struct rf_response rf_rsp;
+
 static const uint rf_num_pins = sizeof(rf_pins) / sizeof(rf_pins[0]);
+static const uint rf_num_leds = sizeof(rf_leds) / sizeof(rf_leds[0]);
 static const uint rf_num_cmds = sizeof(rf_cmds) / sizeof(rf_cmds[0]);
+
+
+void rf_on_led(uint gpio, uint32_t events) {
+
+    (void)events;
+
+    if (gpio == RF_LED_PIN_GREEN)
+        ++rf_rsp.green;
+    else
+        ++rf_rsp.red;
+}
+
 
 static void rf_init_pins(void)
 {
@@ -62,6 +94,15 @@ static void rf_init_pins(void)
         gpio_set_oeover(gpio, GPIO_OVERRIDE_LOW);   /* disable output -> Z state */
         gpio_set_outover(gpio, GPIO_OVERRIDE_LOW);  /* force output low */
         gpio_set_dir(gpio, GPIO_OUT);               /* configure as output */
+    }
+
+    for (uint i = 0; i < rf_num_leds; i++) {
+        uint gpio = rf_leds[i];
+        /* Configure GPIOs for monitoring the LEDs. */
+        gpio_init(gpio);
+        gpio_set_pulls(gpio, true, false);          /* enable pull-up only*/
+        gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_EDGE_FALL, true,
+                &rf_on_led);
     }
 }
 
@@ -76,18 +117,47 @@ static void rf_trigger(uint gpio, uint nump)
     }
 }
 
+static enum rf_result rf_get_response(uint cmd)
+{
+    if (rf_rsp.red > 2)
+        return RF_E_COM;
+
+    if (rf_rsp.red > 1)
+        return RF_E_DEV;
+
+    if (rf_rsp.green != (2 * rf_cmds[cmd].nump))
+        return RF_E_RSP;
+
+    return RF_OK;
+}
+
 static enum rf_result rf_execute(char key)
 {
-    enum rf_result ret = RF_E_CMD;
+    uint cmd;
+    enum rf_result ret;
 
-    for (uint i = 0; i < rf_num_cmds; i++) {
-        if (rf_cmds[i].key == key) {
-            printf("%c: %s\n", key, rf_cmds[i].help);
-            rf_trigger(rf_cmds[i].gpio, rf_cmds[i].nump);
-            ret = RF_OK;
+    /* Clear previous response.*/
+    rf_rsp.green = 0;
+    rf_rsp.red = 0;
+
+    /* Find command. */
+    for (cmd = 0; cmd < rf_num_cmds; cmd++)
+        if (rf_cmds[cmd].key == key)
             break;
-        }
-    }
+
+    if (cmd == rf_num_cmds)
+        return RF_E_CMD;
+
+    printf("request: %s\n", rf_cmds[cmd].help);
+
+    /* Press the button. */
+    rf_trigger(rf_cmds[cmd].gpio, rf_cmds[cmd].nump);
+
+    /* Wait for confirmation. */
+    sleep_ms(RF_DELAY_INDICATION);
+
+    /* Get response. */
+    ret = rf_get_response(cmd);
 
     return ret;
 }
@@ -127,9 +197,13 @@ int main(void)
             enum rf_result ret = rf_execute(c);
             switch (ret) {
             case RF_OK:
+                printf("confirmed\n");
                 break;
             case RF_E_CMD:
                 printf("error: unknown command\n");
+                break;
+            case RF_E_RSP:
+                printf("error: command not confirmed\n");
                 break;
             case RF_E_COM:
                 printf("error: device unreachable\n");
